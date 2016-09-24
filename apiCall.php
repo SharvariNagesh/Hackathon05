9387175296bcd57441d6dfa2ab58b4653e89a5f1
Sharvari Nagesh <sharvari.nagesh@outware.com.au>
2016-09-24 13:39:51 +1000

    With paid key

--- apiCall.php
+++ apiCall.php
@@ -1,24 +1,7 @@
 <?php
-$response = file_get_contents('http://api.smmry.com/SM_API_KEY=06FB4C7AA8&SM_URL=http://www.summarizetool.com/');
+$response = file_get_contents('http://api.smmry.com/SM_API_KEY=F0381A7FDA&SM_URL=http://www.news.com.au/finance/work/leaders/hillary-clinton-vs-donald-trump-presidential-debates-crucial-for-clinton-to-retain-poll-lead/news-story/74e81dbba949c593788ebeb2f54b7f40/');
 $response = json_decode($response, true);
 print $response['sm_api_content'];
 //var_dump($response);
-return;
 
-$long_article = file_get_contents('http://www.summarizetool.com/');
-//echo $long_article;
-$ch = curl_init("http://api.smmry.com/&SM_API_KEY=06FB4C7AA8&SM_LENGTH=14&SM_WITH_BREAK");
-// IMPORTANT!
-curl_setopt($ch, CURLOPT_HTTPHEADER, array("Expect:"));
-// IMPORTANT! Without ^this^ any article over 1000 characters will make SMMRY throw a 417 http error
-curl_setopt($ch, CURLOPT_POST, true); 
-curl_setopt($ch, CURLOPT_POSTFIELDS, "sm_api_input=".$long_article);// Your variable is sent as POST
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
-curl_setopt($ch, CURLOPT_TIMEOUT, 20);
-$return = json_decode(curl_exec($ch), true);//You're summary is now stored in $return['sm_api_content'].
-curl_close($ch);
-
-echo $return['sm_api_content'];
 ?>
