4bcfaf5311a588ecf8a52bdd041ed85cb3d7139a
Loc Nguyen <loc.nguyen@outware.com.au>
2016-09-24 15:59:27 +1000

    1

--- apiCall.php
+++ apiCall.php
@@ -1,8 +1,16 @@
 <?php
 
-$title = $_GET["title"];
-$url = $_GET["url"];
-$type = $_GET["type"];
+// $title = $_GET["title"];
+// $url = $_GET["url"];
+// $type = $_GET["type"];
+
+
+
+
+$title = "Tips On How When And Where To Propose In: Spring";
+$url = "http://www.gq.com.au/lifestyle/the+gq+engagement+guide/tips+on+how+when+and+where+to+propose+in+spring,45233#/%5EeyJocmVmIjoiaHR0cDovL3d3dy5uZXdzLmNvbS5hdS8iLCJhZFVuaXQiOnsic2VydmVyIjoiZGZwLWdwdCIsImVsZW1lbnRJRCI6ImFkLWJsb2NrLTgwNjkwMTgyNzUtMiJ9LCJwcm9wZXJ0eUlkIjoiTkEtTkNBLTExMjM3MzU0IiwibGFiZWwiOiJOQ0EgRGVza3RvcCAtIEhvbWUiLCJzZWxlY3RvciI6Ii50b3Atc3RvcnktZmVlZCAuc3RvcnktYmxvY2s6ZXEoMikiLCJjcmVhdGl2ZSI6ImJjNWY0MDYxMzBlZDQ1OGVhYzQ2ODEwYzJkZjIyYzc5IiwiZXhwZXJpZW5jZVR5cGUiOiJpbmJvdW5kIiwiZXhwZXJpbWVudCI6eyJjcmVhdGl2ZSI6eyJleHBlcmltZW50SWQiOiJkODIzMmVlYjIyMjg0YjAxOTBjMmIyMDJhY2ViOTQ0OSIsIm1vZGVsSWQiOiJjNzI5YzRmYmY5OTY0ZWI5OWYwNjdmYzYxNDQxMjQ2YyJ9fX0=";
+$type = "other";
+
 $response = file_get_contents('http://api.smmry.com/SM_API_KEY=F0381A7FDA&&SM_URL=' . $url);
 $summaryResponse = json_decode($response, true);
 
@@ -24,21 +32,48 @@ else { // default
 }
 
 
-// $title = "Tips On How When And Where To Propose In: Spring";
-// $url = "http://www.gq.com.au/lifestyle/the+gq+engagement+guide/tips+on+how+when+and+where+to+propose+in+spring,45233#/%5EeyJocmVmIjoiaHR0cDovL3d3dy5uZXdzLmNvbS5hdS8iLCJhZFVuaXQiOnsic2VydmVyIjoiZGZwLWdwdCIsImVsZW1lbnRJRCI6ImFkLWJsb2NrLTgwNjkwMTgyNzUtMiJ9LCJwcm9wZXJ0eUlkIjoiTkEtTkNBLTExMjM3MzU0IiwibGFiZWwiOiJOQ0EgRGVza3RvcCAtIEhvbWUiLCJzZWxlY3RvciI6Ii50b3Atc3RvcnktZmVlZCAuc3RvcnktYmxvY2s6ZXEoMikiLCJjcmVhdGl2ZSI6ImJjNWY0MDYxMzBlZDQ1OGVhYzQ2ODEwYzJkZjIyYzc5IiwiZXhwZXJpZW5jZVR5cGUiOiJpbmJvdW5kIiwiZXhwZXJpbWVudCI6eyJjcmVhdGl2ZSI6eyJleHBlcmltZW50SWQiOiJkODIzMmVlYjIyMjg0YjAxOTBjMmIyMDJhY2ViOTQ0OSIsIm1vZGVsSWQiOiJjNzI5YzRmYmY5OTY0ZWI5OWYwNjdmYzYxNDQxMjQ2YyJ9fX0=";
-// $type = "other";
-
-
 function getResponseForDefault($response,$keyword ) {
 	$sentences = explode ('.', $response);
   $sent_dictionary = get_sentences_ranks($sentences, $keyword);
-	if (count($sentences) >= 2) {
-		return $sentences[0] . '.' . $sentences[1] . '.';
-	} elseif (count($sentences) >= 1) {
-		return $sentences[0] . '.';
-	}
 
-	return $response;
+  $bestSentences = array();
+
+  $bestSentenceScore = 0;
+  $bestSentence = '';
+  echo '----0';
+  for ($i=0; $i<2; $i++) {
+  echo '----1';
+      if (count($sentences < 1)) {
+  echo '----2';
+        return '';
+      } elseif (count($sentences < 2)) {
+  echo '----3';
+        return $sentences[0];
+      } elseif (count($sentences < 3)) { 
+  echo '----4';
+        return $sentences[0] . '.' . $sentences[1];
+      }
+  echo '----5';
+
+    foreach ($sentences as $sentence) {
+      $score = $sent_dictionary[$sentence];
+      if ($score > $bestSentenceScore) {
+        $bestSentenceScore = $score;
+        $bestSentence = $sentence;
+      }
+    }
+  echo '----6';
+
+    $bestSentenceScore = 0;
+
+  echo '----7';
+    $bestSentences[] = $bestSentence;
+
+  echo '----8';
+    $sent_dictionary[($bestSentence)] = 0;
+  }
+	
+  return $bestSentences[0] . '.' . $bestSentences[1] . '.';
 }
 
 function get_sentences_ranks($sentences, $keywords) {
@@ -48,7 +83,7 @@ function get_sentences_ranks($sentences, $keywords) {
     $sentences_dic = array();
     for($i = 0;$i < $n;$i++){
       $s1 = $sentences[$i];
-      $sentences_dic[ ( $sentences ) ] = $this->sentences_intersection($s1, $keywords);
+      $sentences_dic[ ( $s1 ) ] = sentences_intersection($s1, $keywords);
     }
     // $this->sentences_dic = $sentences_dic;
     return $sentences_dic;
